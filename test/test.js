b6c708f58d686c892997df93478bea3859475624
leomaurodesenv <leo.mauro.desenv@gmail.com>
2019-03-30 19:44:18 -0300

    fix lib/typography and test issues ~ codacy

--- test/test.js
+++ test/test.js
@@ -5,17 +5,21 @@ const SmmCourseSearch = require("../lib/SmmCourseSearch.js");
 // For example to get the most recents courses
 
 var query = {
-    region: 'all',
-    uploadDate: 'pastDay',
-    sortBy: 'mostRecent'
+    region: "all",
+    uploadDate: "pastDay",
+    sortBy: "mostRecent"
 };
 
-console.log('testing: smm-maker-search');
-console.time('test');
+console.log("testing: smm-maker-search");
+console.time("test");
 SmmCourseSearch.getCourses(query, function(error, courses){
 //SmmCourseSearch.getCoursesPages(query, 1 (firstPage), 10 (lastPage), function(error, courses){
 //SmmCourseSearch.getInfinity(query, function(error, courses){
-    if(error) console.log('> Courses not exist.');
-    else console.log(courses);
-    console.timeEnd('test');
+    if(error) {
+        console.log("> Courses not exist.");
+    }
+    else {
+        console.log(courses);
+    }
+    console.timeEnd("test");
 });
